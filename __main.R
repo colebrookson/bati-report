5b490102ee23792ce5a2a439fb7e6d20118a2c13
colebrookson <cole.brookson@gmail.com>
2024-02-21 02:00:08 -0500

    add some inital code

---
+++ __main.R
@@ -0,0 +1,19 @@
+#' AUTHOR: Cole Brookson
+#' DATE: 2024-02-21
+
+# set up =======================================================================
+
+library(here)
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+fish_data <- readr::read_csv(here::here("./data/bati_fish_data_2023-10-04.csv"))
+seaway_data <- readr::read_csv(
+    here::here("./data/bati_seaway_distances_to_farms.csv")
+)
+site_data <- readr::read_csv(here::here("./data/bati_site_data_2023-10-04.csv"))
+site_master <- readr::read_csv(here::here("./data/bat_site_master_list.csv"))
+farm_activity <- readr::read_csv(here::here("./data/farm_activity.csv"))
+
+#
