1e675b354e6368d87ad98f8aea74a2cfd7a882c1
colebrookson <cole.brookson@gmail.com>
2024-02-23 14:31:26 -0500

    changing main

---
+++ R/01_plots_functions.R
@@ -0,0 +1,9 @@
+#' AUTHOR: Cole Brookson
+#' DATE: 2024-02-21
+
+# make into sf object
+
+plot_study_area <- function(geo_data, site_data) {
+    # make into an sf object first
+    geo_data_sf_bc <- st_as_sf(geo_data)
+}
--- __main.R
+++ __main.R
@@ -7,13 +7,19 @@ library(here)
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(sf)
+
+# read in the functions files
+source(here::here("./R/00_global_funs.R"))
+source(here::here("./R/01_plots_functions.R"))
 
 fish_data <- readr::read_csv(here::here("./data/bati_fish_data_2023-10-04.csv"))
 seaway_data <- readr::read_csv(
     here::here("./data/bati_seaway_distances_to_farms.csv")
 )
 site_data <- readr::read_csv(here::here("./data/bati_site_data_2023-10-04.csv"))
-site_master <- readr::read_csv(here::here("./data/bat_site_master_list.csv"))
-farm_activity <- readr::read_csv(here::here("./data/farm_activity.csv"))
+site_master <- readr::read_csv(here::here("./data/bati_site_master_list.csv"))
+farm_activity <- readxl::read_xlsx(here::here("./data/farm_activity.xlsx"))
+geo_data <- sf::read_sf(here::here("./data/geo-spatial/"))
 
-#
+# put some initial plots =======================================================
