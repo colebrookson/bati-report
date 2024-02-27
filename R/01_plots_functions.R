f95074b9d9fc190211a0b4e72a70645b56dc98d3
colebrookson <cole.brookson@gmail.com>
2024-02-27 13:33:23 -0500

    initial map

--- R/01_plots_functions.R
+++ R/01_plots_functions.R
@@ -1,9 +1,67 @@
 #' AUTHOR: Cole Brookson
 #' DATE: 2024-02-21
 
-# make into sf object
+# make standard map ============================================================
 
-plot_study_area <- function(geo_data, site_data) {
-    # make into an sf object first
-    geo_data_sf_bc <- st_as_sf(geo_data)
+plot_study_area <- function(geo_data, site_data, to_save = TRUE) {
+    # turn shapefile into something workable
+    geo_data_sf_bc <- sf::st_as_sf(geo_data)
+
+    bc_utm <- st_transform(geo_data_sf_bc,
+        crs = "+proj=utm +zone=9 +datum=NAD83 +unit=m"
+    )
+    bc_cropped <- sf::st_crop(bc_utm,
+        xmin = 644000,
+        xmax = 726000,
+        ymin = 5600000,
+        ymax = 5660000
+    )
+
+    # put the farm locations into the proper utm format for the mapping
+    farms_sf <- sf::st_as_sf(farm_locs, coords = c("long", "lat"))
+    sf::st_crs(farms_sf) <- 4326 # set the coordinates for WGS84
+    farms_utm <- sf::st_transform(farms_sf,
+        crs = "+proj=utm +zone=9 +datum=NAD83 +unit=m"
+    ) %>%
+        dplyr::mutate(
+            name = farm_name,
+            area = as.character(NA),
+            type = "farm"
+        ) %>%
+        dplyr::select(name, area, type, geometry)
+
+    # do the same with the sampling locations
+    sampling_sf <- sf::st_as_sf(sampling_locs,
+        coords = c("lon_site_dd", "lat_site_dd")
+    )
+    sf::st_crs(sampling_sf) <- 4326 # set the coordinates for WGS84
+    sampling_utm <- sf::st_transform(sampling_sf,
+        crs = "+proj=utm +zone=9 +datum=NAD83 +unit=m"
+    ) %>%
+        dplyr::mutate(
+            type = "sampling",
+            name = site_name
+        ) %>%
+        dplyr::select(name, area, type, geometry)
+
+    # put the points together and map them
+    all_locs <- rbind(farms_utm, sampling_utm)
+    basic_map <- ggplot() +
+        geom_sf(data = bc_cropped) +
+        geom_sf(data = all_locs, aes(fill = type, shape = type), size = 2) +
+        coord_sf(datum = "+proj=utm +zone=9 +datum=NAD83 +unit=m") +
+        scale_shape_manual("Location", values = c(21, 22)) +
+        scale_fill_manual("Location", values = c("#EA738D", "#89ABE3")) +
+        theme_base() +
+        theme(
+            axis.text = element_blank(),
+            axis.ticks = element_blank(),
+            legend.position = c(0.85, 0.8)
+        )
+    if (to_save) {
+        ggplot2::ggsave(
+            here::here("./figs/maps/basic-map.png"),
+            basic_map
+        )
+    }
 }
--- __main.R
+++ __main.R
@@ -20,6 +20,11 @@ seaway_data <- readr::read_csv(
 site_data <- readr::read_csv(here::here("./data/bati_site_data_2023-10-04.csv"))
 site_master <- readr::read_csv(here::here("./data/bati_site_master_list.csv"))
 farm_activity <- readxl::read_xlsx(here::here("./data/farm_activity.xlsx"))
-geo_data <- sf::read_sf(here::here("./data/geo-spatial/"))
-
+geo_data <- sf::read_sf(here::here("./data/geo-spatial/shapefile/"))
+farm_locs <- readr::read_csv(
+    here::here("./data/geo-spatial/farm-locations.csv")
+)
+sampling_locs <- readr::read_csv(
+    here::here("./data/geo-spatial/bati_site_master_list.csv")
+)
 # put some initial plots =======================================================
